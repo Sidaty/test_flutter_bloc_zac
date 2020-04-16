f78d993b30471d1d426b45c54e53b1567efab565
Sidaty <ksidatym@gmail.com>
2020-04-16 11:42:53 +0000

    Update article_shopping_card_amount.dart

--- lib/ui/article_shopping_card_amount.dart
+++ lib/ui/article_shopping_card_amount.dart
@@ -5,30 +5,31 @@ class ArticleShoppingCardAmount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ArticleBloc, ArticleState>(
-        builder: (context, state) {
-          if (state is ArticleLoadedState) {
-            return Hero(
-              tag: 'amount',
-              child: Material(
-                elevation: 5.0,
-                color: Theme.of(context).primaryColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: SizedBox(
-                  height: 50.0,
-                  width: 150.0,
-                  child: Center(
-                    child: Text(
-                      '${state.totalAmount} FCFA',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
+      builder: (context, state) {
+        if (state is ArticleLoadedState) {
+          return Hero(
+            tag: 'amount',
+            child: Material(
+              elevation: 5.0,
+              color: Theme.of(context).primaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: SizedBox(
+                height: 50.0,
+                width: 150.0,
+                child: Center(
+                  child: Text(
+                    '${state.totalAmount} FCFA',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
                   ),
                 ),
               ),
-            );
-          }
-          return SizedBox();
-        });
+            ),
+          );
+        }
+        return SizedBox();
+      },
+    );
   }
 }
